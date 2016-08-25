8c659daa81599f8dc2b012a34912e90c75ab5717
dlellis@github.com <dlellis@unomaha.edu>
2016-08-24 22:09:44 -0700

    Step 14 stuck spot.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,18 @@ import Ember from 'ember';
 var Photo = Ember.Object.extend({
     title: '',
     username: '',
-    url: '',
+    //flickr extra data
+    owner: '',
+    //flickr url data
+    id: '',
+    farm: 0,
+    secret: '',
+    server: '',
+    url: function(){
+        return "https://farm"+this.get('farm')+
+        ".staticflickr.com/"+this.get('server')+
+        "/"+this.get('id')+"_"+this.get('secret')+"_b+jpg";
+    }.property('farm','server','id','secret'),
 });
 
 var PhotoCollection = Ember.ArrayProxy.extend(Ember.SortableMixin, {
@@ -56,12 +67,38 @@ export default Ember.Controller.extend({
         return photos.filter(function(photo){
             return photo.get('title').match(rx) || photo.get('username').match(rx);
         });
-    }.property('photos','searchField'),
+    }.property('photos.@each','searchField'),
     actions: {
         search: function () {
             this.get('filteredPhotos');
-        }
-    }
+        },
+        getPhotos: function(){
+            var apiKey = 'a9568423bb5acee88c50008f72a33b89';
+            var host = 'https://api.flickr.com/services/rest/';
+            var method = "flicker.tags.getClusterPhotos";
+            var tag = "phones";
+            var requestURL = host + "?method="+method + "&api_key="+apiKey+"&tag="+"&format=json&nojsoncallback=1";
+            var photos = this.get('photos');
+            Ember.$.getJSON(requestURL, function(data){
+                console.log(data);
+                data.photos.photo.map(function(photo) {
+                    var newPhotoItem = Photo.create({
+                        title: photo.title,
+                        username: photo.username,
+                        //flickr extra data
+                        owner: photo.owner,
+                        //flickr url data
+                        id: photo.id,
+                        farm: photo.farm,
+                        secret: photo.secret,
+                        server: photo.server,
+                    
+                    });
+                    photos.pushObject(newPhotoItem);
+            })
+        });
+    },
     
 
+    },
 });
